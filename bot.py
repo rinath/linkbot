1003dc15149430c5bd52b7dcf7b734c9afcae145
rinath <rinatkhn@gmail.com>
2020-02-23 17:03:55 +0600

    working fundamental

--- bot.py
+++ bot.py
@@ -1,3 +1,25 @@
-import telepot
+from chathandler import ChatInstance, ChatHandler
+from telepot.namedtuple import InlineKeyboardButton, InlineKeyboardMarkup
 
-class 
+class Chat(ChatInstance):
+	def __init__(self, bot, chat_id=0):
+		super().__init__(bot, chat_id)
+		self.n = 0
+	def on_message_received(self, msg):
+		self.n += 1
+		self.bot.sendMessage(self.chat_id, f'n={self.n}')
+	def on_command_received(self, command):
+		if command == '/start':
+			s = '''🏻 Привет, я чат-бот курса «Сам себе SMM» от Айжан Мазалиевой!
+⠀
+💪🏻Я рад видеть людей, которые хотят развиваться, достигать целей и всегда открыты новому!
+⠀
+Это фундаментальные принципы,которых мы придерживаемся, и раз ты тут - мы единомышленники💫
+⠀
+Чтобы начать обучение, нажми кнопку « старт»👇🏻'''
+			self.bot.sendMessage(self.chat_id, s)
+	def on_callback_received(self, msg):
+		print('callback:' + msg['data'])
+
+mybot = ChatHandler(Chat, '1002235702:AAHNyEsp3KbkAvfXPygmNNmZ-4BVItaPtWs', 'db.json')
+mybot.start()
---
+++ chathandler.py
@@ -0,0 +1,69 @@
+import telepot
+import time
+from pprint import pprint
+from telepot.loop import MessageLoop
+from tinydb import TinyDB, Query
+from tinydb.middlewares import CachingMiddleware
+from tinydb.storages import JSONStorage
+import sys
+import signal
+
+class ChatHandler:
+	def __init__(self, Chat_class, token, db_filename):
+		self.bot = telepot.Bot(token)
+		self.db = TinyDB(db_filename, storage=CachingMiddleware(JSONStorage))
+		self.db_table = self.db.table('chats')
+		self.chat = Chat_class(self.bot)
+		self.Chat_class = Chat_class
+	def start(self):
+		signal.signal(signal.SIGINT, self.sigint_handler)
+		MessageLoop(self.bot, {'chat': self.handle_message, 'callback_query': self.handle_callback}).run_as_thread()
+		while 1:
+			time.sleep(0.007)
+	def handle_message(self, msg):
+		chat_id = msg['chat']['id']
+		doc_id = self.load(chat_id)
+		if len(msg['text']) > 1 and msg['text'][0] == '/' and msg['text'][1] != ' ':
+			self.chat.on_command_received(msg['text'])
+		else:
+			self.chat.on_message_received(msg)
+		self.backup(doc_id, chat_id)
+	def handle_callback(self, msg):
+		chat_id = msg['message']['chat']['id']
+		doc_id = self.load(chat_id)
+		self.chat.on_callback_received(msg)
+		self.backup(doc_id, chat_id)
+	def load(self, chat_id):
+		q = Query()
+		item = self.db_table.get(q.chat_id == chat_id)
+		if item is None:
+			self.chat = self.Chat_class(self.bot, chat_id)
+			return None
+		else:
+			self.chat.__setstate__(dict(item))
+			return item.doc_id
+	def backup(self, doc_id, chat_id):
+		d = self.chat.__getstate__()
+		if doc_id is None:
+			self.db_table.insert(d)
+		else:
+			self.db_table.write_back(documents=[d], doc_ids=[doc_id])
+	def sigint_handler(self, signum, frame):
+		self.db.close()
+		print('SIGINT caught, closing database...')
+		sys.exit(0)
+
+class ChatInstance:
+	def __init__(self, bot, chat_id=0):
+		self.bot = bot
+		self.chat_id = chat_id
+	def on_message_received(self, msg):
+		raise NotImplementedError
+	def on_command_received(self, msg):
+		raise NotImplementedError
+	def on_callback_received(self, msg):
+		raise NotImplementedError
+	def __getstate__(self):
+		return {k: v for k, v in self.__dict__.items() if k != 'bot'}
+	def __setstate__(self, state):
+		self.__dict__.update(state)
